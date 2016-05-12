b477aa4089c3deec6bbf51ed4478dcc45f215ae7
Mark Williams <markrwilliams@gmail.com>
2016-05-12 00:08:22 -0700

    fix typo in setup.py - this is what happens when you don't use tox

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='txghbot',
-      packages=find_packages() + ['twisted.plugin'],
+      packages=find_packages() + ['twisted.plugins'],
       version='16.0.0',
       install_requires=['Twisted>=16.0.0',
                         'txgithub'])
