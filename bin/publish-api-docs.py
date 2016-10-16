4e93b7d497e3189316cbef734c30929aab05f1a5
Mark Williams <markrwilliams@gmail.com>
2016-10-15 18:24:04 -0700

    script to publish api docs

---
+++ bin/publish-api-docs.py
@@ -0,0 +1,33 @@
+"""
+Run and publish the API docs
+"""
+
+import subprocess
+
+TXGHBOT_MODULE_DIR = "txghbot"
+INTERSPHINX_LINKS = ("https://docs.python.org/2",
+                     "https://docs.python.org/3")
+
+
+def generateDocs():
+    """
+    Generate the API docs.
+    """
+    intersphinxArgs = ['--intersphinx=%s' % (url,)
+                       for url in INTERSPHINX_LINKS]
+    subprocess.check_call(["pydoctor", TXGHBOT_MODULE_DIR] +
+                          intersphinxArgs)
+
+
+def commitToGithubPages():
+    """
+    Commit generated API docs to the `gh-pages` branch.
+    """
+    subprocess.check_call(['git', 'checkout', 'gh-pages'])
+    open('index.html', 'w').close()
+    subprocess.check_call(['git', 'add', 'apidocs'])
+    subprocess.check_call(['git', 'commit', '-m', 'update API docs'])
+
+
+generateDocs()
+commitToGithubPages()
