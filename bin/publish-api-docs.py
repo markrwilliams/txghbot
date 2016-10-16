6c590f35f1352de21b7628406c9b200d2d050034
Mark Williams <markrwilliams@gmail.com>
2016-10-15 18:37:20 -0700

    even betterer doc script

--- bin/publish-api-docs.py
+++ bin/publish-api-docs.py
@@ -2,9 +2,12 @@
 Run and publish the API docs
 """
 import argparse
+import shutil
 import subprocess
 
+
 TXGHBOT_MODULE_DIR = "txghbot"
+APIDOCS = "_apidocs"
 INTERSPHINX_LINKS = (
     "https://docs.python.org/2/objects.inv",
     "https://docs.python.org/3/objects.inv",
@@ -18,7 +21,9 @@ def generateDocs():
     """
     intersphinxArgs = ['--intersphinx=%s' % (url,)
                        for url in INTERSPHINX_LINKS]
-    subprocess.check_call(["pydoctor", TXGHBOT_MODULE_DIR] +
+    subprocess.check_call(["pydoctor",
+                           TXGHBOT_MODULE_DIR,
+                           "--html-output=" + APIDOCS] +
                           intersphinxArgs)
 
 
@@ -27,15 +32,17 @@ def commitToGithubPages():
     Commit generated API docs to the `gh-pages` branch.
     """
     subprocess.check_call(['git', 'checkout', 'gh-pages'])
+    shutil.rmtree('apidocs')
+    shutil.move(APIDOCS, 'apidocs')
     open('index.html', 'w').close()
     subprocess.check_call(['git', 'add', 'index.html'])
     subprocess.check_call(['git', 'add', 'apidocs'])
     subprocess.check_call(['git', 'commit', '-m', 'update API docs'])
     subprocess.check_call(['git', 'checkout', 'master'])
 
-
 parser = argparse.ArgumentParser("generate and maybe publish API docs.")
 parser.add_argument('--publish', '-p',
+                    action='store_true',
                     default=False,
                     help="commit the generated docs to gh-pages and push")
 
